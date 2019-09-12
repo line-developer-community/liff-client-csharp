895ff606cc7d8fcc2e407693a290a38f0127be91
Hirotada Kobayashi <pierre3@outlook.com>
2019-09-12 17:03:53 +0900

    _

--- liff-client-csharp/LiffClient.cs
+++ liff-client-csharp/LiffClient.cs
@@ -32,7 +32,7 @@ namespace LineDC.Liff
         {
             JSRuntime = jSRuntime;
             if (Initialized) { return; }
-            await Task.Delay(1000);
+            ///await Task.Delay(1000);
             var json = await JSRuntime.InvokeAsync<string>("liffInterop.init").ConfigureAwait(false);
             Data = JsonConvert.DeserializeObject<LiffData>(json);
             Initialized = true;
