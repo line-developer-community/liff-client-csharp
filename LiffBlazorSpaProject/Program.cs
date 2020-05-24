51f589ca8dab902ee3a1f9b3f01f99bd3c6d8766
Hirotada Kobayashi <pierre3@outlook.com>
2020-05-24 20:39:06 +0900

    add visual studio template

---
+++ LiffBlazorSpaProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using LineDC.Liff;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            var appSettings = builder.Configuration.Get<AppSettings>();
+            builder.Services.AddSingleton(new HttpClient
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+            builder.Services.AddSingleton<ILiffClient>(new LiffClient(appSettings.LiffId));
+            var host = builder.Build();
+            await host.RunAsync();
+        }
+
+    }
+}
